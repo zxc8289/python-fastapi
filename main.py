faf1d66d5e2875fc3fa0bfcd5b9aa2de64d91430
최철 <zxc8289@naver.com>
2022-11-18 14:28:41 +0900

    fastapi

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException 
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+
+class ResponseDTO(BaseModel):
+    code : int
+    mesege : str
+    data : object
 
 class Cat(BaseModel):
     name: str
@@ -18,3 +25,23 @@ async def second(skip:int = 0, limit: int = 10):
 @app.post("/cat")
 async def cat(cat : Cat):
     return cat
+
+@app.get("/error")
+async def error():
+    dto = ResponseDTO(
+        code=0,
+        mesege="페이지가 없습니다.",
+        data=None
+    )
+    return JSONResponse(status_code=404, content=jsonable_encoder(dto))
+
+    # name = True
+    # if name == True:
+    #     return JSONResponse(status_code=200, content={"message" : "성공"})
+    
+    # else:
+    #     return JSONResponse(status_code=404, content={"message" : "실패"})
+
+@app.get("/error1")
+async def error1():
+    return HTTPException(status_code=404, detail={"message" : "Item not found"})
