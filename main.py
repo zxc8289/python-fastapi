fa461822b960a0c978949f059d133e430d5d6a99
최철 <zxc8289@naver.com>
2022-11-18 11:29:48 +0900

    fastapi

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Cat(BaseModel):
+    name: str
+    id: int
 
 app = FastAPI()
 
+@app.get("/first/{id}")
+async def root(id : int):           
+    return {"message" : "Hello World", "id" : id}
 
+@app.get("/second")
+async def second(skip:int = 0, limit: int = 10):
+    return {"skip" : skip, "limit" : limit}
 
-@app.get("/first")
-async def root():
-    return {"message" : "Hello"}
+@app.post("/cat")
+async def cat(cat : Cat):
+    return cat
